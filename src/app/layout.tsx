0a1c700552db89704e9a3b2547fe0b27454cf0f5
Gabriel Ferreira <gabrielafyasistemas@gmail.com>
2025-07-08 14:19:23 -0400

    adaptando a home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ import dynamic from "next/dynamic";
 import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
-  title: "Luke Berry",
-  description: "Luke Berry's personal website",
+  title: "Gabriel Ferreira",
+  description: "Gabriel Ferreira's personal website",
 };
 
 const karla = Karla({
@@ -43,24 +43,17 @@ export default function RootLayout({
           <div className="flex items-center gap-4">
             <a
               className="decoration-zinc-500 underline-offset-4 transition-all sm:hover:underline dark:decoration-zinc-400"
-              href="https://github.com/LukeberryPi/blog"
+              href="https://github.com/gx-briel/blog"
               target="_blank"
             >
               Code
             </a>
-            <Link
-              className="decoration-zinc-500 underline-offset-4 transition-all sm:hover:underline dark:decoration-zinc-400"
-              target="_blank"
-              href="/rss.xml"
-            >
-              RSS Feed
-            </Link>
             <a
               className="decoration-zinc-500 underline-offset-4 transition-all sm:hover:underline dark:decoration-zinc-400"
-              href="https://x.com/LukeberryPi"
+              href="https://x.com/gx_briel"
               target="_blank"
             >
-              @LukeberryPi
+              @gx_briel
             </a>
           </div>
           <blockquote className="text-zinc-800 dark:text-zinc-300">
